d4470c509bd89e9d4e1256c9de79adfcb2f99355
Pierrick Reille <pierrick11@hotmail.fr>
2022-05-19 16:55:07 +0200

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,8 +10,11 @@ class ListsController < ApplicationController
   end
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+     else
+     render :new
+   end
   end
   private
 
